8135288159b0ed1d734ee48ede0fb84fe7c76fbb
Glitch (spotify-search-demo) <none>
2019-01-27 22:43:44 +0000

    😰👩‍❤️‍💋‍👩 Checkpoint ./server.js:1121985/1610

--- server.js
+++ server.js
@@ -6,19 +6,19 @@ const express = require('express');
 const {getAlbum} = require('./google-photos')
 const app = express();
 
-
+// authorize CORS (for demo only)
 app.use(function(req, res, next) {
   const origin = req.headers.origin;
   if(origin){
-       res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Origin', origin);
   }
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-app.get('/', async function(request, response) {
+app.get('/:id', async function(request, response) {
   try {
-    const results = await getAlbum(request.query.id)
+    const results = await getAlbum(request.params.id)
     response.json(results);
   }
   catch(e) {
