d8dea120392abbc8ee327dc1bd3aee2168109213
Glitch (fabulous-brick-sofa) <none>
2020-03-25 06:08:52 +0000

    📫🎻 Checkpoint ./views/id.ejs:13509542/2347 ./public/client.js:13509542/228

--- public/client.js
+++ public/client.js
@@ -1,3 +1,9 @@
 $(".copy-btn").click(function(e) {
   const url = $(this).parent('div').parent('div').parent('div').attr('id');
 })
+
+function copy(link) {
+  const text =document.getElementById(link);
+  text.select()
+  document.execCommand("copy")
+}
