fd4866c7e80e04ccb2f8c5f27471bd6e0663315e
Glitch (fabulous-brick-sofa) <none>
2020-04-02 09:19:31 +0000

    👓⚽️ Checkpoint ./public/client.js:13509542/989

--- public/client.js
+++ public/client.js
@@ -14,16 +14,28 @@ $("#md").change(function(e) {
 })
 
 function formChange() {
+  let width = $("#width").val();
+  let md = $("#md").prop('checked');
   $(".copy-val").each(function(i) {
     let text = $(this).attr('id');
-    if ($("#width").val()) {
-      text += `=w${$("#width").val()}`
+    if (width) {
+      text += `=w${width}`;
     }
-    if ($("#md").prop('checked')) {
+    if (md) {
       text = `![](${text})`
     }
     $(this).val(text)
   })
+  let query
+  if (width) {
+    query = `?width=${width}`
+    if (!md) {
+      query += `&md=false`
+    }
+  } else if (!md) {
+    query = `?md=false`
+  }
+  
 }
 
 formChange()
